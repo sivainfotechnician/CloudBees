201ea4876f202a61afd9efc74ba6ce12247e6591
SivananthanN <sivananthan.n@hubino.com>
2024-01-25 00:32:24 +0530

    first commit

---
+++ src/main/java/com/codebees/librasync/LibraSyncApplication.java
@@ -0,0 +1,13 @@
+package com.codebees.librasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebees/librasync/LibraSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebees.librasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
